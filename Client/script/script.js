f09e2ded0d08ba696d301194d70f906995311c21
gartgetget <130976170+gartgetget@users.noreply.github.com>
2023-05-25 20:28:58 +0800

    Update script.js
    coded pop up but not working

--- Client/script/script.js
+++ Client/script/script.js
@@ -1,6 +1,11 @@
 //experimentation to add products dynamically
 
 const displayProducts = document.getElementById("prod-list");
+const PopupBox = document.getElementsByClassName("Pop-up");
+//PopupBox Not working yet
+function showbox(){
+    PopupBox.style.display = 'flex';
+}
 
 function addProducts(name){
     const sampleProduct = document.createElement("div");
