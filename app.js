41cd5b4bb93f9ded50ab1fb062de619c9b351933
jmagwili <joshuamagwili@gmail.com>
2023-06-03 11:20:37 +0800

    added backend server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.listen(3000, () => console.log('listening at port 3000'))
+
+app.get('/',(req, res) => {
+    res.render('Login')
+})
+
+
