6040b3cda0eada4d6cc9ade6dd28b0ab6ef5b590
jmagwili <joshuamagwili@gmail.com>
2023-06-03 13:04:48 +0800

    connected mysql database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const menuRoute = require('./controllers/menu')
+const db = require('./database')
 const bodyParser = require('body-parser')
 const app = express()
 
