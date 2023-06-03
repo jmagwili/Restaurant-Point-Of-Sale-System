a2889cf3fe84fd0c27bbd4da3ded6dcc13a2dba6
jmagwili <joshuamagwili@gmail.com>
2023-06-03 11:31:10 +0800

    added login render

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.listen(3000, () => console.log('listening at port 3000'))
 
