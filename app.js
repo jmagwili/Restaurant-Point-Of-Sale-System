4f5189f8b1e198a6f7eb15108114bf8a2d90dd4c
jmagwili <joshuamagwili@gmail.com>
2023-06-05 09:02:27 +0800

    fixed bugs at user authentication

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session');
 const menuRoute = require('./controllers/menu')
 const orderRoute = require('./controllers/orders')
 const db = require('./database')
@@ -14,18 +15,37 @@ app.use((err, req, res, next) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
 })
+app.use(
+    session({
+      secret: 'secret-key',
+      resave: false,
+      saveUninitialized: false,
+    })
+);
+
+module.exports.checkAuthentication = (req, res, next) => {
+    // Check if user is authenticated
+    if (req.session.isAuthenticated) {
+      // User is authenticated, proceed to the dashboard
+      return next();
+    }
+    res.redirect('/');
+}
 
 //routes
-app.use('/menu', menuRoute)
+app.use('/menu', this.checkAuthentication, menuRoute)
 app.use('/orders', orderRoute)
 
 app.get('/',(req, res) => {
+    req.session.destroy()
     res.render('Login')
 })
 
 app.post('/', async (req, res) => {
     const access = db.query('SELECT * FROM users WHERE user_id = ? AND user_password = ?', [req.body.email,req.body.password],(err, results) => {  
         if(results.length === 1){
+            req.session.isAuthenticated = true
+            req.session.username = req.body.email
             res.redirect('/menu')
         }else{
             res.redirect('/')          
--- controllers/menu.js
+++ controllers/menu.js
@@ -1,9 +1,9 @@
 express = require('express')
-
+const app = require('../app')
 menuRoute = express.Router()
 
 menuRoute.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {username: req.session.username})
 })
 
 module.exports = menuRoute
