c97c247ba72d1a4deb4d5ccc096bb80de20cad7d
jmagwili <joshuamagwili@gmail.com>
2023-06-20 17:47:52 +0800

    added db query at user-access route

--- controllers/user_access.js
+++ controllers/user_access.js
@@ -2,8 +2,22 @@ const express = require('express')
 const db = require('../database')
 const userAccessRoute = express.Router()
 
+let usersData
+
+function getUserData(){
+    try{
+        db.query("SELECT * FROM users", (err, results) => {
+            console.log(results)
+            usersData = results    
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
 userAccessRoute.get('/', (req,res) => {
-    res.render('test')
+    getUserData()
+    res.render('test', {usersData})
 })
 
 module.exports = userAccessRoute
